0ae7567cf068ab0a8d18053a4ff98eab4fad702c
shahin <shahinur.rahman.khan10@gmail.com>
2022-05-12 22:01:10 +0600

    all file added

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop_ui/constants.dart';
+import 'package:shop_ui/screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        textTheme: TextTheme(
+            bodyText1: TextStyle(color: kTextColor),
+            bodyText2: TextStyle(color: kTextColor),
+        ),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:shop_ui/components/default_button.dart';
+import 'package:shop_ui/constants.dart';
+import 'package:shop_ui/screens/splash/components/splash_content.dart';
+import 'package:shop_ui/size_config.dart';
+
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Tokoto, Let’s shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text":
+      "We help people conect with store \naround United State of America",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                            (index) => buildDot(index: index),
+                      ),
+                    ),
+                    Spacer(flex: 3),
+                    DefaultButton(
+                      text: "Continue",
+                      press: () {
+                        //Navigator.pushNamed(context, SignInScreen.routeName);
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int? index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
+
+
+// class Body extends StatelessWidget {
+//   const Body({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: SizedBox(
+//         width: double.infinity,
+//         child: Column(
+//           children: [
+//             Expanded(
+//               flex: 3,
+//                 child: Column(
+//                   children: [
+//                     Spacer(),
+//                     Text(
+//                       "TOKOTO",
+//                       style: TextStyle(
+//                         fontSize: getProportionateScreenWidth(36),
+//                         color: kPrimaryColor,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                     Text("Welcome to Tokoto, Let's shop!"),
+//                     Spacer(flex: 2,),
+//                     Image.asset(
+//                       "assets/images/splash_1.png",
+//                       height: getProportionateScreenHeight(265),
+//                       width: getProportionateScreenWidth(235),
+//                     )
+//                   ],
+//                 )
+//             ),
+//             Expanded(
+//               flex: 2,
+//                 child: SizedBox()
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
